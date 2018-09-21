bf0715e91ed71c2a8ab65933359465ecae41cdac
Unknown <devosmaxwell@gmail.com>
2018-09-21 15:34:54 -0500

    Connected GPIO Handler

---
+++ src/GPIOHandler.py
@@ -0,0 +1,27 @@
+def handleOutputs(gameMode, robotEnabled):
+
+    if not robotEnabled:
+        # GPIO Output Code
+        # Not in this version because it won't run on Nik's computer
+        # Hahaha Windows SUX XDXD
+        print("DISABLED")
+
+    else:
+        # GPIO Output Code
+        # Not in this version because it won't run on Nik's computer
+        # Hahaha Windows SUX XDXD
+        print("ENABLED")
+
+    if gameMode == "auto":
+        # GPIO Output Code
+        # Not in this version because it won't run on Nik's computer
+        # Hahaha Windows SUX XDXD
+        print("Auto Mode")
+
+    else:
+        # GPIO Output Code
+        # Not in this version because it won't run on Nik's computer
+        # Hahaha Windows SUX XDXD
+        print("TeleOp Mode")
+
+
