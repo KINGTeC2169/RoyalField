c73053bdd266a4d3b00f4c8f2057ea6e48e6dafb
Max Devos <devosmaxwell@gmail.com>
2018-10-31 10:52:44 -0500

    Updated Client

--- FMS/src/main/java/networkHandler/client/Client.java
+++ FMS/src/main/java/networkHandler/client/Client.java
@@ -1,5 +1,8 @@
 package main.java.networkHandler.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 // Client class
@@ -13,6 +16,21 @@ public class Client extends Thread {
 
     @Override
     public void run() {
+
+        try {
+
+            String data;
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            while (s.isConnected()) {
+                //Check if we've got new data from our client.
+                if ((data = in.readLine()) != null) {
+                    useData(data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
