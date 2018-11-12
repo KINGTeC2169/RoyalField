471870ccce252148757651efe50f94c26bc02553
Max DeVos <devosmaxwell@gmail.com>
2018-11-11 23:02:17 -0600

    Added Match Runnables and States

--- FMS/src/main/java/Main.java
+++ FMS/src/main/java/Main.java
@@ -1,17 +1,24 @@
 package main.java;
 
+import main.java.UI.AudioManager;
+import main.java.UI.UIMain;
+import main.java.UI.text.UIStateMachine;
+import main.java.fms.FMSStates;
 import main.java.fms.match.Match;
 import main.java.networkHandler.NetworkMain;
-import main.java.networkHandler.tabletHandler.TabletManager;
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
 
-        Thread loop = new Thread(Main::loop);
+        Thread loop = new Thread(() -> {
+            while(System.currentTimeMillis() > 0){
+                loop();
+            }
+        });
         loop.start();
 
 
@@ -19,7 +26,7 @@ public class Main {
         NetworkMain.start();
 
         //Launch the UI system
-        //UIMain.start(args);
+        UIMain.start(args);
 
     }
 
@@ -27,36 +34,58 @@ public class Main {
         return 0;
     }
 
-    public static void loop() {
+    private static Match m = null;
 
-        Match m = new Match(1, Match.MatchType.QUAL);
-        /*switch(FMSStates.state){
+    private static void loop() {
+
+        Scanner scan = new Scanner(System.in);
+
+        switch(FMSStates.state){
 
             case INIT:
+                //Wait For Connections until told to advance
+                FMSStates.state = FMSStates.FMSState.PREMATCH;
+                System.out.println("Init Complete");
                 break;
             case PREMATCH:
+                //Check Sensors
+                //Check Tablet Count
+                UIStateMachine.setGameMode("AUTO");
+                while(!scan.hasNext()){
+
+                }
+                System.out.println("Prematch Started");
+                m = new Match(1, Match.MatchType.QUAL);
+                m.start();
                 FMSStates.state = FMSStates.FMSState.MATCH;
+                System.out.println("Prematch Complete");
+                AudioManager.playCharge();
                 break;
             case MATCH:
+                if(m.matchState == Match.MatchState.DONE){
+                    m.stop();
+                    System.out.println("Match Complete");
+                    FMSStates.state = FMSStates.FMSState.VERIFICATION;
+                    break;
+                }
+                m.update();
                 break;
             case VERIFICATION:
+                System.out.println("Awaiting Score Verification");
+                System.out.println("Are these score correct?");
+                while (!scan.nextLine().equals("yes")) {
+                    System.out.println("Are these score correct?");
+                }
+                FMSStates.state = FMSStates.FMSState.POSTMATCH;
                 break;
             case POSTMATCH:
+                System.out.println("Match Completed");
+                System.out.println("*SHOWING RESULTS*");
+                System.out.println("Returning to prematch");
+                FMSStates.state = FMSStates.FMSState.PREMATCH;
                 break;
             default:
                 break;
         }
-        */
-        while(true){
-            m.update();
-            TabletManager.printRobotTablets();
-            TabletManager.printFieldTablets();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
     }
 }
--- FMS/src/main/java/fms/match/Match.java
+++ FMS/src/main/java/fms/match/Match.java
@@ -1,14 +1,24 @@
 package main.java.fms.match;
 
 import main.java.UI.text.UIStateMachine;
+import main.java.fms.scoring.ScoreConstants;
 
 import java.util.StringJoiner;
+import java.util.concurrent.TimeUnit;
 
 public class Match {
 
+    private boolean scoresLocked = false;
     private static int matchID = 0;
     private int matchNum;
     private MatchType matchType;
+    private long startingTime = 0;
+
+    public enum MatchState{
+        AUTO, BREAK, TELE, DONE
+    }
+    public MatchState matchState = MatchState.AUTO;
+
 
     private static int getNewMatchID(){
         matchID++;
@@ -36,6 +46,10 @@ public class Match {
 
     }
 
+    private int getElapsedSeconds(){
+        return (int) TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startingTime);
+    }
+
     public String toString(){
         StringJoiner j = new StringJoiner(",");
         j.add(getNewMatchID() + "");
@@ -46,32 +60,69 @@ public class Match {
         return j.toString();
     }
 
+    public void start(){
+
+        startingTime = System.currentTimeMillis();
+        scoresLocked = false;
+
+        UIStateMachine.setGameMode("AUTO");
+
+    }
+
     public void update(){
+
+        switch(matchState){
+
+            case AUTO:
+                if(getElapsedSeconds() >= ScoreConstants.autoDuration){
+                    matchState = MatchState.BREAK;
+                    break;
+                }
+                break;
+            case BREAK:
+                System.out.println("Teleop Starting");
+                UIStateMachine.setGameMode("TELE");
+                matchState = MatchState.TELE;
+                break;
+            case TELE:
+                if(getElapsedSeconds() >= (ScoreConstants.autoDuration + ScoreConstants.teleDuration)) {
+                    matchState = MatchState.DONE;
+                }
+                break;
+            case DONE:
+                stop();
+        }
+
         blue.calculateTotalScore();
         red.calculateTotalScore();
+        updateScoreBoard();
     }
 
-    public void updateScoreBoard(){
+    public void stop(){
 
-        //Update Blue Values
-        UIStateMachine.setBlueScore(blue.getTotalScore());
-        UIStateMachine.setBlueRelic(blue.getRelics());
-        UIStateMachine.setBlueFlag(blue.getFlags());
-        UIStateMachine.setBluePingPong(blue.getMoonRocks());
-
-        //Update Red Values
-        UIStateMachine.setRedScore(red.getTotalScore());
-        UIStateMachine.setRedRelic(red.getRelics());
-        UIStateMachine.setRedFlag(red.getFlags());
-        UIStateMachine.setRedPingPong(red.getMoonRocks());
+        scoresLocked = true;
 
     }
 
-    public Alliance getBlue() {
-        return blue;
+    private void updateScoreBoard(){
+
+        if(!scoresLocked){
+
+            //Update Time
+            UIStateMachine.setTime((ScoreConstants.autoDuration + ScoreConstants.teleDuration) - getElapsedSeconds());
+
+            //Update Blue Values
+            UIStateMachine.setBlueScore(blue.getTotalScore());
+            UIStateMachine.setBlueRelic(blue.getRelics());
+            UIStateMachine.setBlueFlag(blue.getFlags());
+            UIStateMachine.setBluePingPong(blue.getMoonRocks());
+
+            //Update Red Values
+            UIStateMachine.setRedScore(red.getTotalScore());
+            UIStateMachine.setRedRelic(red.getRelics());
+            UIStateMachine.setRedFlag(red.getFlags());
+            UIStateMachine.setRedPingPong(red.getMoonRocks());
+        }
     }
 
-    public Alliance getRed() {
-        return blue;
-    }
 }
