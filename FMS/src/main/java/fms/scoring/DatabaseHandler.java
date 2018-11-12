88c0c95a8bc1c2feef377230823559da81cbc9a8
Max Devos <devosmaxwell@gmail.com>
2018-11-12 10:14:08 -0600

    Added ScoreBase

---
+++ FMS/src/main/java/fms/scoring/DatabaseHandler.java
@@ -0,0 +1,30 @@
+package main.java.fms.scoring;
+
+import main.java.fms.match.Match;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DatabaseHandler {
+
+
+    public FileWriter createPW(){
+        try {
+            return new FileWriter("bin/data/MatchResults.csv",true);
+        } catch (IOException e) {
+            System.out.println("Error Reading MatchResults.csv! Is it in /bin/data/?");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void archiveMatch(Match m){
+        FileWriter out = createPW();
+    }
+
+    public MatchTemplate readMatch(){
+
+    }
+
+
+}
